af0191b85f95b3c48b52d4cc28a8deb2b3717470
abaynew-guadie <abaynewhu@yahoo.com>
2023-10-18 04:45:24 -0700

    task 0-5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,32 +1,34 @@
 #!/usr/bin/env python3
-""" Redis Module """
+""" expiring web cache module """
 
-from functools import wraps
 import redis
 import requests
 from typing import Callable
+from functools import wraps
 
-redis_ = redis.Redis()
+redis = redis.Redis()
 
 
-def count_requests(method: Callable) -> Callable:
-    """ Decortator for counting """
-    @wraps(method)
-    def wrapper(url):  # sourcery skip: use-named-expression
-        """ Wrapper for decorator """
-        redis_.incr(f"count:{url}")
-        cached_html = redis_.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-        html = method(url)
-        redis_.setex(f"cached:{url}", 10, html)
-        return html
+def wrap_requests(fn: Callable) -> Callable:
+    """ Decorator wrapper """
+
+    @wraps(fn)
+    def wrapper(url):
+        """ Wrapper for decorator guy """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
 
     return wrapper
 
 
-@count_requests
+@wrap_requests
 def get_page(url: str) -> str:
-    """ Obtain the HTML content of a  URL """
-    req = requests.get(url)
-    return req.text
+    """get page self descriptive
+    """
+    response = requests.get(url)
+    return response.text
