01e8a0f9f9c6094ec9644aee2e7be4c59a555283
abaynew-guadie <abaynewhu@yahoo.com>
2023-10-19 08:00:54 -0700

    task 0-5

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,39 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
-import redis
-import requests
+
 from functools import wraps
+import redis
+from requests import get
 from typing import Callable
 
 
-redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+redis_client = redis.Redis()
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
-    @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
-        result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+def responsed_cached_or_not(fn: Callable) -> Callable:
+    """
+    A simple decorator to cache a http request in redis
+    """
+    @wraps(fn)
+    def wrapper(url):
+        """
+        The wrapper function which gets returned
+        by the decorator
+        """
+        redis_client.incr(f"count:{url}")
+        cached_response = redis_client.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis_client.setex(f"cached:{url}", 10, result)
         return result
-    return invoker
+
+    return wrapper
 
 
-@data_cacher
+@responsed_cached_or_not
 def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
-    return requests.get(url).text
+    """
+    A simple function to make http requests
+    to a certain endpoint
+    """
+    return get(url).text
